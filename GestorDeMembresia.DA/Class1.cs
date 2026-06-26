c15609fa5a2f541caacd742e955b9928713d74eb
FIORELLA.DELAGUARDIA@ucr.ac.cr <FIORELLA.DELAGUARDIA@ucr.ac.cr>
2026-06-26 17:28:38 -0600

    Creacion de la clase membresia e implementacion a la capa MODEL

--- GestorDeMembresia.DA/Class1.cs
+++ GestorDeMembresia.DA/Class1.cs
@@ -1,7 +1,18 @@
-﻿namespace GestorDeMembresia.DA
+﻿namespace GestorDeMembresia.Model
 {
-    public class Class1
+    public class Membresia
     {
-
+        public int Id { get; set; }
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellidos { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public decimal PrecioBaseMensual { get; set; }
+        public int TipoDePlan { get; set; }   
+        public DateTime FechaDeInicio { get; set; }
+        public DateTime FechaDeVencimiento { get; set; }
+        public decimal MontoTotal { get; set; }
+        public int Estado { get; set; }       
     }
 }
+
